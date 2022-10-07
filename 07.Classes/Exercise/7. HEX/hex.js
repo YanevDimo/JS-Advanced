1db7e9e62b33b3dc90287a390dc0088c2a88f561
Dimo Yanev <dimocz@gmail.com>
2022-10-07 15:11:22 +0200

    js-advanced

---
+++ 07.Classes/Exercise/7. HEX/hex.js
@@ -0,0 +1,50 @@
+class Hex {
+    constructor(value) {
+        this.value = value;
+    }
+
+    valueOf() {
+        return this.value;
+    }
+
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase();
+    }
+
+    plus(number) {
+
+        if (Number(number)) {
+            return new Hex(this.value + number.value);
+        }
+
+        return new Hex(this.value + parseInt(number.toString(), 16));
+    }
+
+    minus(number) {
+        if (Number(number)) {
+            return new Hex(this.value - number.value);
+        }
+
+        return new Hex(this.value - parseInt(number.toString(), 16));
+    }
+
+    parse(hexValue) {
+        return parseInt(hexValue, 16);
+    }
+}
+
+let FF = new Hex(255);
+
+console.log(FF.toString());
+
+FF.valueOf() + 1 === 256;
+
+let a = new Hex(10);
+
+let b = new Hex(5);
+
+console.log(a.plus(b).toString());
+
+console.log(a.plus(b).toString() === '0xF');
+
+console.log(FF.parse('AAA'));
