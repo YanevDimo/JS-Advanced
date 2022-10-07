591729155ebc82fb2407c3b819690946dbcf1953
Dimo Yanev <dimocz@gmail.com>
2022-10-07 16:29:40 +0200

    js-advanced

--- 07.Classes/Exercise/9. Auto-Engineering Company/autoInginerungCompany.js
+++ 07.Classes/Exercise/9. Auto-Engineering Company/autoInginerungCompany.js
@@ -1,26 +1,43 @@
-function autoEngineeringCompany(input){
+// function autoEngineeringCompany(input){
+//
+//     let result = {};
+//
+//     for (const line of input) {
+//         let [brand, model, qnty] = line.split(' | ');
+//         if(!result[brand]){
+//             result[brand] = {};
+//         }
+//         if(!result[brand][model]){
+//             result[brand][model] = 0
+//         }
+//         result[brand][model] += Number(qnty);
+//     }
+//     for (const key in result) {
+//         console.log(key);
+//         for (const model of Object.keys(result[key])) {
+//             console.log(`###${model} -> ${result[key][model]}`);
+//         }
+//     }
+// }
+function autoEngineeringCompany(input) {
+    let colection = new Map();
 
-    let result = {};
-
-    for (const line of input) {
-        let [brand, model, qnty] = line.split(' | ');
-
-        if(!result[brand]){
-            result[brand] = {};
+    for (let line of input) {
+        let [brand, model, productCars] = line.split(' | ');
+        productCars = Number(productCars);
+        if (!colection.has(brand)) {
+            colection.set(brand, new Map());
         }
-
-        if(!result[brand][model]){
-            result[brand][model] = 0
+        if (colection.get(brand).has(model)) {
+            colection.get(brand).set(model, colection.get(brand).get(model) + productCars);
+        } else {
+            colection.get(brand).set(model, productCars);
         }
-
-        result[brand][model] += Number(qnty);
     }
-
-    for (const key in result) {
-        console.log(key);
-
-        for (const model of Object.keys(result[key])) {
-            console.log(`###${model} -> ${result[key][model]}`);
+    for (let [brand, models] of colection.entries()) {
+        console.log(brand);
+        for (let [model, productCars] of models.entries()) {
+            console.log(`###${model} -> ${productCars}`);
         }
     }
 }
